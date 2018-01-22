a68c60243361e57363c88300bc00dc27acb0a24e
xuweicheng <xuweicheng@onlyedu.com>
2018-01-22 10:34:12 +0800

    优化

--- app/src/main/java/com/example/administrator/lifecycleframework/binding/BindingAdapters.java
+++ app/src/main/java/com/example/administrator/lifecycleframework/binding/BindingAdapters.java
@@ -8,6 +8,7 @@ import android.view.View;
  */
 public class BindingAdapters {
 
+
     @BindingAdapter("visibleGone")
     public static void showHide(View view, boolean show) {
         view.setVisibility(show ? View.VISIBLE : View.GONE);
